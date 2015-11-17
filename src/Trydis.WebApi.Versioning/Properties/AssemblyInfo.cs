1cd9853cf0b60d1953beff035fc78677383d29ff
Morten Trydal <trydis@hotmail.com>
2015-11-17 16:23:58 +0100

    Build script

--- src/Trydis.WebApi.Versioning/Properties/AssemblyInfo.cs
+++ src/Trydis.WebApi.Versioning/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Trydis.WebApi.Versioning")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Versioned routes and models.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("trydis")]
 [assembly: AssemblyProduct("Trydis.WebApi.Versioning")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © trydis 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+// [assembly: AssemblyVersion("0.1.0.0")]
+// [assembly: AssemblyFileVersion("0.1.0.0")]
